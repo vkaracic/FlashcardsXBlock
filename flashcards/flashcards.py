ec8d22c15707dc5faf3aeba2c49fdcf462aa37fd
vkaracic <karacicvedran@gmail.com>
2015-06-10 16:09:36 +0200

    added a closing bracket

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -47,7 +47,7 @@ class FlashcardsXBlock(XBlock):
 
         frag = Fragment()
         template = env.get_template("flashcards.html")
-        frag.add_content(template.render(**context)
+        frag.add_content(template.render(**context))
         frag.add_css(self.resource_string("static/css/flashcards.css"))
         frag.add_javascript(self.resource_string("static/js/src/flashcards.js"))
         frag.initialize_js('FlashcardsXBlock')
